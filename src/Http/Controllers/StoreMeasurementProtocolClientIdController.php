8325a4d19a71fde7a63f47ece80dba7b811a6a24
tjardoo <tjardoo@users.noreply.github.com>
2023-12-05 15:08:17 +0000

    Fix styling

--- src/Http/Controllers/StoreMeasurementProtocolClientIdController.php
+++ src/Http/Controllers/StoreMeasurementProtocolClientIdController.php
@@ -7,7 +7,7 @@ class StoreMeasurementProtocolClientIdController
     public function __invoke(): void
     {
         session([
-            config('tagmanager.measurement_protocol_client_id_session_key') => request('client_id')
+            config('tagmanager.measurement_protocol_client_id_session_key') => request('client_id'),
         ]);
     }
 }
